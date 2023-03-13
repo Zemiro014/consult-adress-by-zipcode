99c753b418353273a3556c28c20b841e68f73459
Jeronimo Kulandissa <jeronimo.kulandissa@opah.com.br>
2023-03-12 22:18:43 -0300

    First commit

---
+++ challangeZipCode/src/main/java/jeronimo/wipro/challangeZipCode/ChallangeZipCodeApplication.java
@@ -0,0 +1,13 @@
+package jeronimo.wipro.challangeZipCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallangeZipCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallangeZipCodeApplication.class, args);
+	}
+
+}
---
+++ challangeZipCode/src/test/java/jeronimo/wipro/challangeZipCode/ChallangeZipCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package jeronimo.wipro.challangeZipCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallangeZipCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
