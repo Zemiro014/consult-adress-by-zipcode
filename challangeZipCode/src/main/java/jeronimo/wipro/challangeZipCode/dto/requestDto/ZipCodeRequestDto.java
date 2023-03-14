8dedc10815f2942aa22c0ebd79477ce4297f196b
Jeronimo Kulandissa <jeronimo.kulandissa@opah.com.br>
2023-03-14 01:44:05 -0300

    Setting openapi document

--- challangeZipCode/src/main/java/jeronimo/wipro/challangeZipCode/dto/requestDto/ZipCodeRequestDto.java
+++ challangeZipCode/src/main/java/jeronimo/wipro/challangeZipCode/dto/requestDto/ZipCodeRequestDto.java
@@ -6,11 +6,19 @@ import lombok.Setter;
 
 import java.io.Serializable;
 
-@Data
-@Setter
-@Getter
+//@Data
+//@Setter
+//@Getter
 public class ZipCodeRequestDto implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private String cep;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
 }
--- challangeZipCode/src/main/java/jeronimo/wipro/challangeZipCode/dto/responsesDto/AdressResponseDto.java
+++ challangeZipCode/src/main/java/jeronimo/wipro/challangeZipCode/dto/responsesDto/AdressResponseDto.java
@@ -6,23 +6,74 @@ import lombok.Setter;
 
 import java.io.Serializable;
 
-@Data
-@Setter
-@Getter
+//@Data
+//@Setter
+//@Getter
 public class AdressResponseDto implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private String cep;
     private String rua;
     private String complemento;
-
     private String bairro;
-
     private String cidade;
-
     private String estado;
-
     private float frete;
 
 
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public float getFrete() {
+        return frete;
+    }
+
+    public void setFrete(float frete) {
+        this.frete = frete;
+    }
 }
